cd9aae399ca3a37642273948abb05fc243f66af7
Manikandan Sundararajan <mani@hey.com>
2023-05-31 00:13:50 -0400

    basic thread stuff

--- src/main.rs
+++ src/main.rs
@@ -15,5 +15,27 @@ use std::{
 };
 
 fn main() {
-    println!("Hello, world!");
+    // let numbers = vec![1, 2, 3];
+    let numbers = Vec::from_iter(0..=1000);
+
+    let t = thread::spawn(move || {
+        let len = numbers.len();
+        let sum = numbers.iter().sum::<usize>();
+
+        sum / len
+    });
+
+    println!("Hello from the main thread");
+
+    // Waits until the thread has finished executing and returns a std::thread::Result
+    let avg = t.join().unwrap();
+    println!("avg = {avg}");
+    // t2.join().unwrap();
+}
+
+fn _f() {
+    println!("Hello from other thread");
+
+    let id = thread::current().id();
+    println!("This is my thread id: {id:?}");
 }
